a857555308f0dfdd91747b5110160c369e8c00fb
dinamohammed <dina.mohammed392@gmail.com>
2020-07-01 09:59:00 +0000

    Add Module + add button view

--- delivery_history/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ delivery_history/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale_management','stock','egymentors_purchase_fx'],
+    'depends': ['base','sale_management','stock','egymentors_purchase_fx','sale'],
 
     # always loaded
     'data': [
