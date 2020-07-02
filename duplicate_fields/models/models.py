4e82dc813db17c50ff96cecd7152e10a7d9eeb9e
hamzamohamed651233 <hamza.mohamed1238@gmail.com>
2020-07-02 12:30:19 +0000

    Sale order duplicated fields

--- duplicate_fields/models/models.py
+++ duplicate_fields/models/models.py
@@ -18,6 +18,37 @@ from odoo import models, fields, api
 #             record.value2 = float(record.value) / 100
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-        
-    name=fields.Char('Name')
-        
+    
+    advanced_amounted =fields.Monetary(related='payment_history_ids.advance_amount')
+    advanced_amounted_y =fields.Float(compute='_compute_amount_advanced_amount', store = True)
+    
+    due_amount = fields.Float(compute='_compute_amount_total_due', store = True)
+    
+    @api.depends('advanced_amounted','amount_total')
+    def _compute_amount_total_due(self):
+        for record in self:
+            record.due_amount = record.amount_total - record.advanced_amounted
+            
+    
+    return_pay = fields.Float(compute='_return_pay', store = True)
+    
+    @api.depends('order_line.qty_delivered','order_line.price_unit')
+    def _return_pay(self):
+        delivery_line_t = 0
+        for record in self:
+            for line in record.order_line:
+                if line.qty_delivered:
+                    delivery_line = (line.product_uom_qty -line.qty_delivered) * line.price_unit
+                    delivery_line_t = delivery_line_t + delivery_line
+            record.return_pay = delivery_line_t
+                
+    
+    @api.depends('payment_history_ids.advance_amount')
+    def _compute_amount_advanced_amount(self):
+        advance_amount_line_t = 0
+        for record in self:
+            for line in record.payment_history_ids:
+                advance_amount_line = line.advance_amount
+                advance_amount_line_t = advance_amount_line_t + advance_amount_line
+            record.advanced_amounted_y = advance_amount_line_t
+                
