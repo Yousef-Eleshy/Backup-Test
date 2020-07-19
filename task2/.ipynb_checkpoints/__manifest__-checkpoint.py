da8ef8201fc6e0ea23363e15bcd4971c76bc8d1c
hatem1055 <odoo@eupp13.odoo.com>
2020-07-19 12:02:44 +0000

    /

--- task2/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ task2/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -26,7 +26,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/forcastsaleline.xml',
+        'views/sale_order_line.xml',
     ],
     # only loaded in demonstration mode
    
---
+++ task2/models/sale_order_line.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError,ValidationError
+from odoo.osv import expression
+from odoo.tools.float_utils import float_round
+from datetime import datetime
+import operator as py_operator
+
+class ForcastQty(models.Model):
+    _inherit = 'sale.order.line'
+    
+    virtual_available = fields.Float(related='product_id.virtual_available', readonly=True,string='Available Quantity')
+    batch = fields.Many2many('stock.quant',string='Batch')
