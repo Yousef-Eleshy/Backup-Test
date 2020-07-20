51ed03e2509182079cac9558296eb7f9e9b50da0
hatem1055 <odoo@eupp13.odoo.com>
2020-07-20 20:16:32 +0000

    add state domain and solve multy company proplems

--- task2/models/.ipynb_checkpoints/sale_order_line-checkpoint.py
+++ task2/models/.ipynb_checkpoints/sale_order_line-checkpoint.py
@@ -11,28 +11,30 @@ class ForcastQty(models.Model):
     _rec_name = 'name'
     
     virtual_available = fields.Float(related='product_id.virtual_available', readonly=True,string='Available Quantity')
-    batch = fields.Many2many('stock.quant',string='Batch',check_company=True)
+    batch = fields.Many2many('stock.quant',string='Batch',company_dependent=False,check_company=False)
     
     @api.onchange('batch')
     def batch_quantity(self):
         qty = 0
         for batch in self.batch:
             qty += batch.lot_id.product_qty 
+            batch.company_dependent = False
+            batch.check_company=False
         self.product_uom_qty = qty
 
     
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+# class SaleOrder(models.Model):
+#     _inherit = 'sale.order'
     
-    def action_confirm(self):
-        for rec in self:
-            lines = rec.order_line
-            for line in lines:
-                batches = line.batch
-                for batch in batches:
-                    batch.write({'state':'reserved'})
+#     def action_confirm(self):
+#         raise ValidationError('sasasaas')
+#         for rec in self:
+#             lines = rec.order_line
+#             for line in lines:
+#                 batches = line.batch
+#                 for batch in batches:
+#                     batch.write({'state':'reserved'})
         
-        
-        return super(SaleOrderLine, self)
+#         return super(SaleOrder, self).action_confirm()
 
         
--- task2/models/.ipynb_checkpoints/stock_quant-checkpoint.py
+++ task2/models/.ipynb_checkpoints/stock_quant-checkpoint.py
@@ -31,3 +31,32 @@ class StockQuant(models.Model):
             qty = rec.lot_id.product_qty 
 
             rec.name = f'{batch} || {lot} || {qty}' 
+    @api.model
+    def create(self, vals):
+        """ Override to handle the "inventory mode" and create a quant as
+        superuser the conditions are met.
+        """
+        if self._is_inventory_mode() and 'inventory_quantity' in vals:
+            allowed_fields = self._get_inventory_fields_create()
+            if any([field for field in vals.keys() if field not in allowed_fields]):
+                raise UserError(_("Quant's creation is restricted, you can't do this operation."))
+            inventory_quantity = vals.pop('inventory_quantity')
+
+            # Create an empty quant or write on a similar one.
+            product = self.env['product.product'].browse(vals['product_id'])
+            location = self.env['stock.location'].browse(vals['location_id'])
+            lot_id = self.env['stock.production.lot'].browse(vals.get('lot_id'))
+            package_id = self.env['stock.quant.package'].browse(vals.get('package_id'))
+            owner_id = self.env['res.partner'].browse(vals.get('owner_id'))
+            quant = self._gather(product, location, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=True)
+            if quant:
+                quant = quant[0]
+            else:
+                quant = self.sudo().create(vals)
+            # Set the `inventory_quantity` field to create the necessary move.
+            quant.inventory_quantity = inventory_quantity
+            return quant
+        res = super(StockQuant, self).create(vals)
+#         if self._is_inventory_mode():
+#             res._check_company()
+        return res
