c1106495af9d6a9528c29da3b587ef9320b61304
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-07-20 09:59:05 +0200

    Update sale_order_line.py

--- task2/models/sale_order_line.py
+++ task2/models/sale_order_line.py
@@ -8,6 +8,7 @@ import operator as py_operator
 
 class ForcastQty(models.Model):
     _inherit = 'sale.order.line'
+    _rec_name = 'name'
     
     virtual_available = fields.Float(related='product_id.virtual_available', readonly=True,string='Available Quantity')
     batch = fields.Many2many('stock.quant',string='Batch')
