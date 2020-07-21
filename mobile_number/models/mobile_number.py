7ee5231077e573efd4ebe7ebe248c8176de70436
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-07-21 14:28:21 +0000

    Mobile Number Validation

--- mobile_number/models/mobile_number.py
+++ mobile_number/models/mobile_number.py
@@ -7,19 +7,27 @@ class Partner(models.Model):
     _inherit = 'res.partner'
 
 
-
+    #When the Country is Egypt
     @api.constrains('mobile')
     def _onchange_mobile(self):
         if self.country_id.name == 'Egypt':
             self.onchange_mobile()
+            
     #Only numbers and 11 (16 including spaces) digits only
     def onchange_mobile(self):
         if self.mobile:
             if len(self.mobile) != 16:
-                raise ValidationError(_("Enter valid 11 digits Mobile number"))
+                raise ValidationError(_("WARNING: Please Enter a Valid 11 Digit Mobile Number"))
         digits = [' ','+','0','1','2','3','4','5','6','7','8','9']
         for i in list(self.mobile):
             if i in digits:
                 continue
             else:
-                raise ValidationError("Attention !! Mobile number %s , should contains only numbers" % (self.mobile))
+                raise ValidationError("WARNING: Mobile Numbers %s , Should only contain Numbers" % (self.mobile))
+    
+    # Make sure the Mobile Number is Unique
+    _sql_constraints = [
+                     ('mobile', 
+                      'unique(mobile)',
+                      'WARNING: Another Contact with this Mobile Number Already Exists')
+    ]
