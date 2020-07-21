417e1b86fd54a4fe8715ab3ea572b7833bac7e5a
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-07-21 15:48:18 +0000

    Attempt to remove spaces

--- mobile_search/models/res_partner.py
+++ mobile_search/models/res_partner.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, api
-
+from odoo import models, api,_
+from odoo.exceptions import ValidationError, AccessError
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     @api.model
     def name_search(self, name='', args=None, operator='ilike', limit=100):
-        """ This method will find Customer names according to its mobile,
+        """ This method will find Customer names according to their mobile,
         phone, city, email and its job position."""
         if name and not self.env.context.get('import_file'):
+            #name.replace(" ","")
+            #name = [i.replace(",", "") for i in name]
+            
             args = args if args else []
             args.extend(['|', ['name', 'ilike', name],
                          '|', ['mobile', 'ilike', name],
