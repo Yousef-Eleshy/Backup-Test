25f2971f7bb02131293d97e07b2caf055ec95319
hatem1055 <odoo@eupd2.odoo.com>
2020-07-27 16:56:42 +0000

    /

--- restapi/controllers/controllers.py
+++ restapi/controllers/controllers.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
-from odoo import http
+from odoo import http,fields,models
 from odoo.http import request
 import io
 import base64
 class Restapi(http.Controller):
+     def prepare_allowed_companies(self,user_id):
+            user = request.env['res.users'].search([('id','=',user_id)])
+            ids = [company.id for company in user.company_id]
+            return ids
+        
      @http.route('/web/session/create_customer',type='json',auth='none')
-     def getauthentication(self,db,username,password,full_name,country_code,mobile,email,base_location=None):
-        request.session.authenticate(db,username,password)
+     def create_customer(self,full_name,country_code,mobile,email,base_location=None):
         country = request.env['res.country'].search([('code','=',country_code)],limit=1).id
         vals = {
             'name':full_name,
@@ -17,10 +21,11 @@ class Restapi(http.Controller):
         }
         request.env['res.partner'].create(vals)
         return {
-            'result':'created successfully'
+            'created successfully'
         }
+    
      @http.route('/web/session/edit_customer',type='json',auth='none')
-     def edit_customer(self,db,username,password,customer_id,full_name,country_code,mobile,email,base_location=None):
+     def edit_customer(self,customer_id,full_name,country_code,mobile,email,base_location=None):
         request.session.authenticate(db,username,password)
         country = request.env['res.country'].search([('code','=',country_code)],limit=1).id
         customer = request.env['res.partner'].search([('id','=',customer_id)],limit=1)
@@ -32,30 +37,29 @@ class Restapi(http.Controller):
         }
         customer.write(vals)
         return {
-            'result':'edited successfully',
+            'edited successfully',
         }
     
      @http.route('/web/session/popular_products',type='json',auth='none')
-     def popular_products(self,db,username,password,base_location=None):
-        request.session.authenticate(db,username,password)
+     def popular_products(self,base_location=None):
         result = []
-        products = request.env['product.template'].search([])
+        products = request.env['product.template'].sudo().search([('name','=','Test')])
         for product in products:
             image = product.image_1920
             availability = 'In Stock' if product.virtual_available > 0 else 'Out Of Stock'
             vals = {
                 'product_id':product.id,
-                'image':base64.b64decode(image) if image else '',
                 'product_name':product.name,
                 'product_code':product.default_code,
                 'price':product.list_price,
-                'availability':availability
+                'availability':availability,
+                'image':base64.b64decode(image) if image else '',
             }
             result.append(vals)
         return result
     
      @http.route('/web/session/list_customers',type='json',auth='none')
-     def list_customers(self,db,username,password,base_location=None):
+     def list_customers(self,base_location=None):
         request.session.authenticate(db,username,password)
         result = []
         customers = request.env['res.partner'].search([('customer_rank','=',True)])
@@ -69,5 +73,47 @@ class Restapi(http.Controller):
             }
             result.append(vals)
         return result
+     @http.route('/web/session/sales_home',type='json',auth='none')
+     def sales_home(self,user_id,base_location=None):
+        result = []
+        allowed_companies = self.prepare_allowed_companies(user_id)
+        sales = request.env['sale.order'].sudo().search([('company_id','in',allowed_companies)])
+        sales_today = [sale for sale in sales if sale.date_order.day == fields.date.today().day and sale.date_order.month == fields.date.today().month and sale.date_order.year == fields.date.today().year]
+        sales_yesterday = [sale for sale in sales if sale.date_order.day == fields.date.today().day - 1 and sale.date_order.month == fields.date.today().month and sale.date_order.year == fields.date.today().year]
+        sales_today_sum = 0
+        sales_yesterday_sum = 0
+        for sale in sales_today:
+            sales_today_sum += sale.amount_total
+        for sale in sales_yesterday:
+            sales_yesterday_sum += sale.amount_total
+        perc = ((sales_today_sum - sales_yesterday_sum) / sales_yesterday) * 100 if sales_yesterday_sum != 0 else 100
+        return {
+            'today_sales_amount':sales_today_sum,
+            'today_sales_percentage':perc,
+            'draft_notes':[]
+        }
+    
+     @http.route('/web/session/scan_product',type='json',auth='none')
+     def scan_product(self,code,base_location=None):
+        product = request.env['product.product'].sudo().search([('barcode','=',code)],limit=1)
+        info = {
+            'name':product.name,
+            'code':product.barcode,
+            'price':product.list_price,
+            'description':product.description,
+            'stock_availability ':product.virtual_available,
+            'product_id':product.id
+        }
+        return  info
+    
+     @http.route('/web/session/forget_password',type='json',auth='none')
+     def forget_password(self,email,base_location=None):
+        email = 'hatemmostafa31@gmail.com'
+        request.session.authenticate('yousef-eleshy-mentors-devs-task2-api-1312735',email,'admin')
+        user = request.env['res.users'].search([('login','=',email)],limit=1)
+        user.sudo().action_reset_password()
+        return  'resend a reset password to user mail'                                          
+                       
+                                                         
 
     
