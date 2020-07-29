886128676d800894634c09388f472a24755f9cb3
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-07-29 14:26:09 +0000

    Editable Footer Done

--- egymentors_sales_enhancement/models/specs_conditions.py
+++ egymentors_sales_enhancement/models/specs_conditions.py
@@ -9,12 +9,12 @@ class SpecsConditions(models.Model):
     
     specs_conditions = fields.Html(string='Offer Specifications and Conditions', required=True, default="""
                                     <ul>
-                                    <li>All prices are in Egyptian pound LE and subject to 14% vat.</li>
+                                    <li><b>All prices are in Egyptian pound LE and subject to 14% vat.</b></li>
                                     <li>Please note any fabric marked WRF are 100% flam ratadant polyster durable for 12 to 16 wash to match and pass the highest required American standard NFPA701</li>
                                     <li>All original fabric is construction engineered to match the project durability use and safety requirements.</li>
                                     <li>All fabrics made to pass aatcc 16e (U.S.A) Xeon test 5 blue scale minimum (for light fastness)</li>
                                     <li>Fabric specifications for indoor use only</li>
                                     <li>All fabrics made to pass aatcc 8 (U.S.A) for dry and wet grey scale (crocking fastness rubbing)</li>
                                     <li>All upholstery fabrics made to pass astm 4966 Abrasion test for upholstery items (20000 rubs minimum)</li>
-                                    <li>Ordered qty subject for (5% + or -) on delivery</li>""")
+                                    <p>Ordered qty subject for (5% + or -) on delivery</p>""")
     
