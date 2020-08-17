e9c8b2c3313af289b6bf6ed0acb3813002c82ce2
hatem1055 <odoo@eupd2.odoo.com>
2020-08-17 09:54:02 +0000

    some fixes for mobile app

--- mobile_number/models/.ipynb_checkpoints/mobile_number-checkpoint.py
+++ mobile_number/models/.ipynb_checkpoints/mobile_number-checkpoint.py
@@ -17,7 +17,7 @@ class Partner(models.Model):
     #Only numbers and 11 (16 including spaces) digits only
     def onchange_mobile(self):
         if self.mobile:
-            if len(self.mobile) != 16:
+            if len(self.mobile) != 16 and len(self.mobile) != 11:
                 raise ValidationError(_("WARNING: Please Enter a Valid 11 Digit Mobile Number"))
         digits = [' ','+','0','1','2','3','4','5','6','7','8','9']
         for i in list(self.mobile):
@@ -26,7 +26,7 @@ class Partner(models.Model):
             else:
                 raise ValidationError("WARNING: Mobile Numbers %s , Should only contain Numbers" % (self.mobile))
     
-    # Make sure the Mobile Number is Unique
+#     Make sure the Mobile Number is Unique
 #     _sql_constraints = [
 #                      ('mobile', 
 #                       'unique(mobile)',
@@ -37,4 +37,4 @@ class Partner(models.Model):
     def mobile_unique_constrain(self):
         for partner in self:
             if partner.ref and self.env['res.partner'].search_count([('mobile', '=', partner.mobile)]) > 1:
-                raise Warning(_("WARNING: Another Contact with this Mobile Number Already Exists"))
+                raise ValidationError(_("WARNING: Another Contact with this Mobile Number Already Exists"))
